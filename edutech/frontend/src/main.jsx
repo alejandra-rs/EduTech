2b610fb134d0c8ef8e5cb6a74b4ece0538e23bd1
studies-ulpgc <dacil.santana108@alu.ulpgc.es>
2026-04-02 22:31:59 +0100

    PS-23 feat: restored course template

--- edutech/frontend/src/main.jsx
+++ edutech/frontend/src/main.jsx
@@ -1,10 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './prueba.jsx'
+import Course from './components/Subject.jsx'
+import { WidgetCourse } from './components/Course.jsx'
+
+/*
 const root = createRoot(document.getElementById('root'))
 root.render(
   <StrictMode>
-    <App />
+    <WidgetCourse />
+  </StrictMode>,
+)
+*/
+
+const root = createRoot(document.getElementById('root'))
+root.render(
+  <StrictMode>
+    <WidgetCourse />
   </StrictMode>,
 )
