9ad1f960f9974622490ec2a7475acf5a189afaa1
SaturnAco <h.wrks097@gmail.com>
2026-04-02 20:20:47 +0200

    PS-36 feat: added coments

--- edutech/frontend/src/main.jsx
+++ edutech/frontend/src/main.jsx
@@ -2,11 +2,20 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './prueba.jsx'
-import { WidgetCourse } from './components/Course.jsx'
+//import { WidgetCourse } from './components/Course.jsx'
 
+/*
 const root = createRoot(document.getElementById('root'))
 root.render(
   <StrictMode>
     <WidgetCourse />
+  </StrictMode>,
+)
+*/
+
+const root = createRoot(document.getElementById('root'))
+root.render(
+  <StrictMode>
+    <App />
   </StrictMode>,
 )
