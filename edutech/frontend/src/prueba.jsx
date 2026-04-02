d5f73a80f5a69ec7a69b4e3ae068ecebaec4a560
SaturnAco <h.wrks097@gmail.com>
2026-04-02 23:17:04 +0200

    PS-67 feat: added VisorVideo

--- edutech/frontend/src/prueba.jsx
+++ edutech/frontend/src/prueba.jsx
@@ -1,17 +1,13 @@
-import VisorPDF from './components/VisorPDF'
+import VisorVideo from './components/VisorVideo'
 
 export default function App() {
-  const pdfPrueba = "https://arxiv.org/pdf/2203.15556.pdf";
+  const miVideo = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"; 
 
   return (
-    <div className="min-h-screen w-full bg-gray-100 p-4 md:p-10 flex flex-col items-center">
-      
-      <h1 className="text-2xl font-bold text-gray-800 mb-8">
-        Vista Previa (Proporción A4)
-      </h1>
+    <div className="min-h-screen w-full bg-zinc-100 p-4 md:p-10 flex flex-col items-center">
 
-      <div className="w-full max-w-3xl">
-        <VisorPDF pdfUrl={pdfPrueba} />
+      <div className="w-full max-w-4xl">
+        <VisorVideo videoUrl={miVideo} />
       </div>
 
     </div>
