93bbd6c51af7b43a9d014956d3be72600bab140b
4w4kt <carrona43@gmail.com>
2026-04-02 23:53:56 +0100

    PS-8 feat: refine PostCard component props and clean up Layout component comments

--- edutech/frontend/src/components/Layout.jsx
+++ edutech/frontend/src/components/Layout.jsx
@@ -2,16 +2,12 @@ Layout.jsx
 import { useState } from 'react';
 import Header from './Header';
 
-// La propiedad { children } representa todo lo que pongas DENTRO de las etiquetas <Layout> ... </Layout>
 export default function Layout({ children }) {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
     <div className="min-h-screen bg-gray-100 flex">
-      {/* El Header siempre estará aquí, manejando su propio estado de abierto/cerrado */}
       <Header isOpen={isOpen} setIsOpen={setIsOpen} userProfilePic={null} userName={null} />
-      
-      {/* El main ajusta su margen automáticamente, y luego imprime lo que sea que le pasemos */}
       <main className={`flex-1 p-8 transition-all duration-300 ${isOpen ? 'ml-64' : 'ml-24'}`}>
         {children}
       </main>
