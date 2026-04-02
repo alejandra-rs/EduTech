c324197d29d38a693f0256c3c74622c513e9aba6
4w4kt <carrona43@gmail.com>
2026-04-02 16:46:13 +0100

    PS-8 feat: frontend structure

---
+++ edutech/frontend/src/prueba.jsx
@@ -0,0 +1,14 @@
+import Header from './components/Header'
+
+export default function App() {
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <Header />
+      <main className="p-8">
+        <p className="text-gray-700 text-lg text-center mt-10">
+          ¡Genial! Tu estructura de componentes ya está funcionando.
+        </p>
+      </main>
+    </div>
+  )
+}
---
+++ edutech/frontend/vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite'
+// Importamos el nuevo plugin de SWC
+import react from '@vitejs/plugin-react-swc'
+import tailwindcss from '@tailwindcss/vite'
+// https://vite.dev/config/
+export default defineConfig({
+  
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
+})
