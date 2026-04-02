9be6fbacfcced1d649f02f8df1f2d190042858dc
studies-ulpgc <dacil.santana108@alu.ulpgc.es>
2026-04-02 18:48:31 +0100

    PS-23 feat: added course template

--- edutech/frontend/src/main.jsx
+++ edutech/frontend/src/main.jsx
@@ -2,10 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './prueba.jsx'
+import { WidgetCourse } from './components/Course.jsx'
 
 const root = createRoot(document.getElementById('root'))
 root.render(
   <StrictMode>
-    <App />
+    <WidgetCourse />
   </StrictMode>,
 )
