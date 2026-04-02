20441a6736b838b83e069cdeefb9d727d2fe6aae
studies-ulpgc <dacil.santana108@alu.ulpgc.es>
2026-04-03 00:16:49 +0100

    PS-23 feat: fixed layout

--- edutech/frontend/src/components/Layout.jsx
+++ edutech/frontend/src/components/Layout.jsx
@@ -1,4 +1,3 @@
-Layout.jsx
 import { useState } from 'react';
 import Header from './Header';
 
@@ -6,9 +5,10 @@ export default function Layout({ children }) {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <div className="min-h-screen bg-gray-100 flex">
+    <div className="min-h-screen bg-gray-100 flex overflow-x-hidden">
       <Header isOpen={isOpen} setIsOpen={setIsOpen} userProfilePic={null} userName={null} />
-      <main className={`flex-1 flex flex-col min-h-screen transition-all duration-300 ${isOpen ? 'ml-64' : 'ml-24'}`}>
+      
+      <main className={`flex-1 flex flex-col min-h-screen transition-all duration-300`}>
         {children}
       </main>
     </div>
