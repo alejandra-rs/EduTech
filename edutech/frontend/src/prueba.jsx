3f267944e380f565d44037e6fb7e06f55994fb61
SaturnAco <h.wrks097@gmail.com>
2026-04-03 22:46:42 +0200

    PS-45 feat: prueba.jsx con navegación

--- edutech/frontend/src/prueba.jsx
+++ edutech/frontend/src/prueba.jsx
@@ -1,26 +1,19 @@
-import Input from './components/input';
+import Layout from './components/Layout';
+import Courses from './pages/AllCourses'; 
+import Subject from './pages/AllSubjects';
+import SubjectDetail from './pages/SubjectDetail';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-export default function Prueba() {
+export default function App() {
   return (
-    <div className="min-h-screen bg-gray-50 p-10 flex flex-col items-center gap-10">
-      <div className="w-full max-w-2xl bg-white p-8 rounded-2xl shadow-sm space-y-8">
-        {/* Ejemplo 1: Subida de Archivos */}
-        <Input 
-          label="Sube tu documento PDF" 
-          type="file" 
-        />
-        {/* Ejemplo 2: Título (Input normal) */}
-        <Input 
-          label="Título de la publicación" 
-          placeholder="Ej: Apuntes Tema 1" 
-        />
-        {/* Ejemplo 3: Descripción (Textarea) */}
-        <Input 
-          label="Descripción detallada" 
-          placeholder="Escribe aquí los detalles..." 
-          rows={4} 
-        />
-      </div>
-    </div>
+    <BrowserRouter>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Courses />} />
+          <Route path="/:id/asignaturas" element={<Subject />} />
+          <Route path="/:id/:subjectId/post" element={<SubjectDetail />} />
+        </Routes>
+      </Layout>
+    </BrowserRouter>
   );
 }
