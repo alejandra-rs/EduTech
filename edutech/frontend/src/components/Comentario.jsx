ff7ace84064b5384fff667e739672806f9a5b7d9
SaturnAco <h.wrks097@gmail.com>
2026-04-04 23:16:41 +0200

    feat: updated comentario

--- edutech/frontend/src/components/Comentario.jsx
+++ edutech/frontend/src/components/Comentario.jsx
@@ -1,3 +1,6 @@
+import Like from './Like';
+import Dislike from './Dislike';
+
 export default function Comentario() {
   return (
     <div className="bg-[#dfdfdf] rounded-lg p-5 sm:p-6 flex gap-4 w-full font-sans">
@@ -9,7 +12,6 @@ export default function Comentario() {
       />
       
       <div className="flex-1 flex flex-col">
-        
         <h3 className="text-xl font-semibold text-gray-900 mb-2">
           Name Surname
         </h3>
@@ -23,19 +25,8 @@ export default function Comentario() {
         
         <div className="flex justify-end items-center gap-5 mt-auto">
           
-          <button className="flex items-center gap-2 text-gray-900 hover:opacity-70 transition-opacity">
-            <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-              <path d="M14 9V5a3 3 0 0 0-3-3l-4 9v11h11.28a2 2 0 0 0 2-1.7l1.38-9a2 2 0 0 0-2-2.3zM7 22H4a2 2 0 0 1-2-2v-7a2 2 0 0 1 2-2h3"></path>
-            </svg>
-            <span className="font-bold text-lg">100</span>
-          </button>
-          
-          <button className="flex items-center gap-2 text-gray-900 hover:opacity-70 transition-opacity">
-            <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-              <path d="M10 15v4a3 3 0 0 0 3 3l4-9V2H5.72a2 2 0 0 0-2 1.7l-1.38 9a2 2 0 0 0 2 2.3zm7-13h3a2 2 0 0 1 2 2v7a2 2 0 0 1-2 2h-3"></path>
-            </svg>
-            <span className="font-bold text-lg">50</span>
-          </button>
+          <Like />
+          <Dislike />
           
           <button className="text-gray-900 hover:opacity-70 transition-opacity ml-2">
             <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round">
