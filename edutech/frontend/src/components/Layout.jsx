4499fed07aa51d30d9a40a48cfaa5bdbfb4351db
Cris <75226837+Cristina-SM@users.noreply.github.com>
2026-04-04 14:00:44 +0100

    feat: integrate NotebookFooter and update routing for CargarPublicacionVideo

--- edutech/frontend/src/components/Layout.jsx
+++ edutech/frontend/src/components/Layout.jsx
@@ -1,17 +1,57 @@
-import { useState } from 'react';
-import Header from './Header';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { use, useState } from "react";
+import Header from "./Header";
+import NotebookFooter from "./Footer";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 
 export default function Layout({ children }) {
   const [isOpen, setIsOpen] = useState(false);
 
+  const tabs_config = {
+    default: [
+      { label: "Mis asignaturas", color: "bg-red-400" },
+      { label: "Mi espacio", color: "bg-yellow-400" },
+      { label: "Todos", color: "bg-green-400" },
+    ],
+    "/cargarPublicacion": [
+      // Si la URL de cargarPublicacionPDF o Video cambia debe el nombre
+      { label: "PDF", color: "bg-red-400" },
+      { label: "Video", color: "bg-blue-400" },
+    ],
+  };
+
+  const location = useLocation();
+  const currentTabs =
+    tabs_config[
+      location.pathname.endsWith("/cargarPublicacion")
+        ? "/cargarPublicacion" // Si la URL de cargarPublicacionPDF o Video cambia debe actualizar esta condición, minimo debe contender o terminar con esto
+        : "default"
+    ];
+
   return (
-    <div className="min-h-screen bg-gray-100 flex overflow-x-hidden">
-      <Header isOpen={isOpen} setIsOpen={setIsOpen} userProfilePic={null} userName={null} />
-      
-      <main className={`flex-1 flex flex-col min-h-screen transition-all duration-300`}>
-        {children}
-      </main>
+    <div className="flex h-screen w-full bg-slate-200 overflow-hidden">
+      <Header
+        isOpen={isOpen}
+        setIsOpen={setIsOpen}
+        userProfilePic={null}
+        userName={null}
+      />
+      <div className="flex flex-1 flex-col relative overflow-hidden">
+        <main
+          className={`flex-1 bg-white overflow-y-auto custom-scrollbar 
+             transition-all duration-300`}
+        >
+          <div className="min-h-full flex flex-col pb-10">
+            <div className="flex-1">{children}</div>
+            <div
+              className="h-10 shrink-0 pointer-events-none"
+              aria-hidden="true"
+            />{" "}
+          </div>
+        </main>
+        <div className="absolute bottom-0 left-0 w-full z-20 bg-transparent">
+          <NotebookFooter tabs={currentTabs} />
+        </div>
+      </div>
     </div>
   );
 }
--- edutech/frontend/src/prueba.jsx
+++ edutech/frontend/src/prueba.jsx
@@ -1,8 +1,9 @@
-import Layout from './components/Layout';
-import Courses from './pages/AllCourses'; 
-import Subject from './pages/AllSubjects';
-import SubjectDetail from './pages/SubjectDetail';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Layout from "./components/Layout";
+import Courses from "./pages/AllCourses";
+import Subject from "./pages/AllSubjects";
+import SubjectDetail from "./pages/SubjectDetail";
+import CargarPublicacionVideo from "./pages/CargarPublicacionVideo";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 export default function App() {
   return (
@@ -12,6 +13,10 @@ export default function App() {
           <Route path="/" element={<Courses />} />
           <Route path="/:id/asignaturas" element={<Subject />} />
           <Route path="/:id/:subjectId/post" element={<SubjectDetail />} />
+          <Route
+            path="/:id/:subjectId/cargarPublicacion"
+            element={<CargarPublicacionVideo />}
+          />
         </Routes>
       </Layout>
     </BrowserRouter>
