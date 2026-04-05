5fdc0ad5a07d06980560f2f5850e74408786a486
SaturnAco <h.wrks097@gmail.com>
2026-04-05 15:49:03 +0200

    PS-23 feat: updated footer for CargarPublicacion

--- edutech/frontend/src/prueba.jsx
+++ edutech/frontend/src/prueba.jsx
@@ -3,9 +3,10 @@ import Courses from './pages/AllCourses';
 import Subject from './pages/AllSubjects';
 import SubjectDetail from './pages/SubjectDetail';
 import CargarPublicacionPDF from './pages/CargarPublicacionPDF';
+import CargarPublicacionVideo from './pages/CargarPublicacionVideo';
 import VistaPreviaDocumento from './pages/VistaPreviaDocumento';
 import VistaPreviaVideo from './pages/VistaPreviaVideo';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
 export default function App() {
   return (
@@ -15,7 +16,9 @@ export default function App() {
           <Route path="/" element={<Courses />} />
           <Route path="/:id/asignaturas" element={<Subject />} />
           <Route path="/:id/:subjectId/post" element={<SubjectDetail />} />
-          <Route path="/:id/:subjectId/upload" element={<CargarPublicacionPDF />} />
+          <Route path="/:id/:subjectId/upload" element={<Navigate to="PDF" replace />} />
+          <Route path="/:id/:subjectId/upload/PDF" element={<CargarPublicacionPDF />} />
+          <Route path="/:id/:subjectId/upload/Video" element={<CargarPublicacionVideo />} />
           <Route path="/:id/:subjectId/documento/:postId" element={<VistaPreviaDocumento />} />
           <Route path="/:id/:subjectId/video/:postId" element={<VistaPreviaVideo />} />
         </Routes>
