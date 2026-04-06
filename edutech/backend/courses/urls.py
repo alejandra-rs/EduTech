5596b8077798110b6165dbd2a16ba7d63bfa60bd
Pooul14 <alejandra.rodriguezsilva2000@gmail.com>
2026-04-06 13:16:56 +0100

    fix: added endpoints for integration with frontend.

--- edutech/backend/courses/urls.py
+++ edutech/backend/courses/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('<int:pk>', views.CourseDetailView.as_view(), name='course_detail'),
     path('years/', views.YearListCreate.as_view(), name='list_years'),
     path('sub/', views.SubscriptionView.as_view(), name='subscription'),
     path('sub/<int:pk>', views.SubscriptionView.as_view(), name='sub_delete'),
--- edutech/backend/courses/views.py
+++ edutech/backend/courses/views.py
@@ -15,13 +15,19 @@ class YearListCreate(generics.ListCreateAPIView):
 class CourseListCreate(generics.ListCreateAPIView):
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
-    filterset_fields = ['year']
+    filterset_fields = ['year', 'semester']
 
     def list(self, request, *args, **kwargs):
         year_id = request.query_params.get('year')
         if year_id is not None:
             get_object_or_404(Year, pk=year_id)
         return super().list(request, *args, **kwargs)
+
+
+class CourseDetailView(views.APIView):
+    def get(self, request, pk):
+        course = get_object_or_404(Course, pk=pk)
+        return Response(CourseSerializer(course).data, status=status.HTTP_200_OK)
 
 
 class SubscriptionView(views.APIView):
