53160faba4710ceb3c22370ab290fded2e887a0e
Cris <75226837+Cristina-SM@users.noreply.github.com>
2026-04-30 13:54:05 +0100

    feat: user update degree profile

--- edutech/backend/users/urls.py
+++ edutech/backend/users/urls.py
@@ -4,5 +4,6 @@ from .views import StudentView, StudentListView
 urlpatterns = [
     path("<int:pk>/", StudentView.as_view()),
     path("post/", StudentView.as_view(), name="create_student"),
+    path("path/<int:pk>", StudentView.as_view(), name="update_student"),
     path("", StudentListView.as_view(), name="list_students"),
 ]
